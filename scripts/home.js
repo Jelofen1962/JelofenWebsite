e4053665bf94f8c7471cfbc8b1399bd24b894133
Jelofen1962 <amirrezataati1962@gmail.com>
2024-11-03 23:37:36 +0330

    Mobile fixed

--- scripts/home.js
+++ scripts/home.js
@@ -21,5 +21,18 @@ buttons.forEach(button => {
 
 var scale = 'scale(1)';
 document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
- document.body.style.msTransform =   scale;       // IE 9
- document.body.style.transform = scale;     // General
+document.body.style.msTransform =   scale;       // IE 9
+document.body.style.transform = scale;     // General
+
+function menu_open() {
+    const navButtons = document.getElementsByClassName('navbutton');
+  
+    // Toggle the display of navButtons based on their current state
+    for (let i = 0; i < navButtons.length; i++) {
+      if (navButtons[i].style.display === 'none') {
+        navButtons[i].style.display = 'unset';
+      } else {
+        navButtons[i].style.display = 'none';
+      }
+    }
+  }
